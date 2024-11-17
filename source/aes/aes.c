2336c5304cd5d6c3540786b2f680d1b923e8d368
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2024-11-17 19:02:49 +0100

    Fixed bug in aes/aes.c that files under 16 bytes couldn't be encrypted.

--- source/aes/aes.c
+++ source/aes/aes.c
@@ -4,7 +4,7 @@
 /*
  * SubBytes
  */
-void sub_bytes(uint8_t block[16])
+static void sub_bytes(uint8_t block[16])
 {
   for(uint8_t index = 0; index < 16; index++)
   {
@@ -15,7 +15,7 @@ void sub_bytes(uint8_t block[16])
 /*
  * SubBytes - inverse
  */
-void sub_bytes_inverse(uint8_t block[16])
+static void sub_bytes_inverse(uint8_t block[16])
 {
   for(uint8_t index = 0; index < 16; index++)
   {
@@ -23,7 +23,7 @@ void sub_bytes_inverse(uint8_t block[16])
   }
 }
 
-void bytes_switch(uint8_t block[16], uint8_t a, uint8_t b)
+static void bytes_switch(uint8_t block[16], uint8_t a, uint8_t b)
 {
   uint8_t temp = block[a];
 
@@ -35,7 +35,7 @@ void bytes_switch(uint8_t block[16], uint8_t a, uint8_t b)
 /*
  * ShiftRows
  */
-void shift_rows(uint8_t block[16])
+static void shift_rows(uint8_t block[16])
 {
   bytes_switch(block, 4, 5);
   bytes_switch(block, 4, 6);
@@ -52,7 +52,7 @@ void shift_rows(uint8_t block[16])
 /*
  * ShiftRows - inverse
  */
-void shift_rows_inverse(uint8_t block[16])
+static void shift_rows_inverse(uint8_t block[16])
 {
   bytes_switch(block, 4, 7);
   bytes_switch(block, 4, 6);
@@ -72,7 +72,7 @@ void shift_rows_inverse(uint8_t block[16])
 /*
  * MixColumns
  */
-void mix_columns(uint8_t block[16])
+static void mix_columns(uint8_t block[16])
 {
   for(uint8_t column = 0; column < 4; column++)
   {
@@ -91,7 +91,7 @@ void mix_columns(uint8_t block[16])
 /*
  * MixColumns - inverse
  */
-void mix_columns_inverse(uint8_t block[16])
+static void mix_columns_inverse(uint8_t block[16])
 {
   for(uint8_t column = 0; column < 4; column++)
   {
@@ -110,7 +110,7 @@ void mix_columns_inverse(uint8_t block[16])
 /*
  * AddRoundKey
  */
-void add_round_key(uint8_t block[16], const uint8_t rkey[16])
+static void add_round_key(uint8_t block[16], const uint8_t rkey[16])
 {
   for(uint8_t index = 0; index < 16; index++)
   {
@@ -121,7 +121,7 @@ void add_round_key(uint8_t block[16], const uint8_t rkey[16])
 /*
  *
  */
-void aes_block_encrypt(uint8_t result[16], const uint8_t input[16], const uint8_t* rkeys, uint8_t rounds)
+static void aes_block_encrypt(uint8_t result[16], const uint8_t input[16], const uint8_t* rkeys, uint8_t rounds)
 {
   uint8_t block[16];
   memcpy(block, input, 16);
@@ -151,7 +151,7 @@ void aes_block_encrypt(uint8_t result[16], const uint8_t input[16], const uint8_
 /*
  *
  */
-void aes_block_decrypt(uint8_t result[16], const uint8_t input[16], const uint8_t* rkeys, uint8_t rounds)
+static void aes_block_decrypt(uint8_t result[16], const uint8_t input[16], const uint8_t* rkeys, uint8_t rounds)
 {
   uint8_t block[16];
   memcpy(block, input, 16);
@@ -179,61 +179,87 @@ void aes_block_decrypt(uint8_t result[16], const uint8_t input[16], const uint8_
 }
 
 /*
+ * Encrypt message using AES key of different sizes
+ *
  * RETURN (int status)
- * - 0 | Success!
+ * - 0 | Success
  * - 1 | Failed to expand key
  */
 int aes_encrypt(void* result, const void* message, size_t size, const char* key, ksize_t ksize)
 {
+  // 1. Expand the key
   uint8_t rounds = ROUND_KEYS(ksize);
 
   uint32_t rkeys[4 * rounds];
-  if(key_expand(rkeys, (uint32_t*) key, ksize) != 0) return 1;
+
+  if(key_expand(rkeys, (uint32_t*) key, ksize) != 0)
+  {
+    return 1;
+  }
 
   size_t index;
   uint8_t block[16];
 
-  for(index = 0; index < (size - 16); index += 16)
+  // 2. Encrypt the whole blocks in message
+  for(index = 0; index + 16 <= size; index += 16)
   {
     memcpy(block, (uint8_t*) message + index, 16);
 
     aes_block_encrypt(result + index, block, (uint8_t*) rkeys, rounds);
   }
 
-  memset(block, 0, 16);
-  memcpy(block, (uint8_t*) message + index, size - index);
+  // 3. Encrypt the rest of the message
+  if(index < size)
+  {
+    memset(block, 0, 16);
 
-  aes_block_encrypt(result + index, block, (uint8_t*) rkeys, rounds);
+    memcpy(block, (uint8_t*) message + index, size - index);
 
-  return 0; // Success!
+    aes_block_encrypt(result + index, block, (uint8_t*) rkeys, rounds);
+  }
+
+  return 0;
 }
 
 /*
+ * Decrypt AES message using key of different sizes
+ *
  * RETURN (int status)
- * - 0 | Success!
+ * - 0 | Success
  * - 1 | Failed to expand key
  */
 int aes_decrypt(void* result, const void* message, size_t size, const char* key, ksize_t ksize)
 {
+  // 1. Expand the key
   uint8_t rounds = ROUND_KEYS(ksize);
 
   uint32_t rkeys[4 * rounds];
-  if(key_expand(rkeys, (uint32_t*) key, ksize) != 0) return 1;
+
+  if(key_expand(rkeys, (uint32_t*) key, ksize) != 0)
+  {
+    return 1;
+  }
 
   size_t index;
   uint8_t block[16];
 
-  for(index = 0; index < (size - 16); index += 16)
+  // 2. Decrypt the whole blocks in message
+  for(index = 0; index + 16 <= size; index += 16)
   {
     memcpy(block, (uint8_t*) message + index, 16);
 
     aes_block_decrypt(result + index, block, (uint8_t*) rkeys, rounds);
   }
 
-  memset(block, 0, 16);
-  memcpy(block, (uint8_t*) message + index, size - index);
+  // 3. Decrypt the rest of the message
+  if(index < size)
+  {
+    memset(block, 0, 16);
 
-  aes_block_decrypt(result + index, block, (uint8_t*) rkeys, rounds);
+    memcpy(block, (uint8_t*) message + index, size - index);
 
-  return 0; // Success!
+    aes_block_encrypt(result + index, block, (uint8_t*) rkeys, rounds);
+  }
+
+  return 0;
 }
