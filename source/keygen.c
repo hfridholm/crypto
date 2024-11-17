9009ec1fe5ac68f229f2db8212e7105b1c8bdb8a
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2024-11-17 21:18:02 +0100

    Rewrote key generation in rsa.c and renamed it to rsa-key.c.

--- source/keygen.c
+++ source/keygen.c
@@ -11,26 +11,30 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <stdlib.h>
-
-const char SKEY_FILE[] = "key";
-const char PKEY_FILE[] = "key.pub";
-
-char dir[128] = "./";
-int bytes = 4096;
 
 int main(int argc, char* argv[])
 {
-  char skey[4096];
-  memset(skey, '\0', sizeof(skey));
+  srand(time(NULL));
 
-  char pkey[4096];
-  memset(pkey, '\0', sizeof(pkey));
+  skey_t skey;
+  pkey_t pkey;
 
-  rsa_base64_create(skey, pkey);
+  keys_generate(&skey, &pkey);
 
-  printf("Private key:\n%s\n", skey);
-  printf("Public key:\n%s\n", pkey);
+  printf("pkey:\n");
+  gmp_printf("n: %Zd\n", pkey.n);
+  gmp_printf("e: %Zd\n", pkey.e);
+
+  printf("skey:\n");
+  gmp_printf("n: %Zd\n", skey.n);
+  gmp_printf("e: %Zd\n", skey.e);
+  gmp_printf("d: %Zd\n", skey.d);
+  gmp_printf("p: %Zd\n", skey.p);
+  gmp_printf("q: %Zd\n", skey.q);
+
+  keys_free(&skey, &pkey);
 
   return 0;
 }
--- source/rsa.h
+++ source/rsa.h
@@ -1,8 +1,35 @@
+/*
+ * rsa.h
+ *
+ * Written by Hampus Fridholm
+ *
+ * Last updated: 2024-11-17
+ */
+
 #ifndef RSA_H
 #define RSA_H
 
+#include <stdlib.h>
 #include <gmp.h>
+#include <stdio.h>
 
-extern void rsa_base64_create(char* skey, char* pkey);
+typedef struct
+{
+  mpz_t n; // Modulus
+  mpz_t e; // Public exponent
+} pkey_t;
+
+typedef struct
+{
+  mpz_t n; // Modulus
+  mpz_t e; // Public exponent
+  mpz_t d; // Private exponent
+  mpz_t p; // Prime p
+  mpz_t q; // Prime q
+} skey_t;
+
+extern int  keys_generate(skey_t* skey, pkey_t* pkey);
+
+extern void keys_free(skey_t* skey, pkey_t* pkey);
 
 #endif // RSA_H
